838196cec761cc3ec4d88ea6d18fc951db491348
MostafaIn <mostafa.hazareh@integrify.fi>
2019-07-04 12:51:46 +0300

    Updates

---
+++ precache-manifest.bcb10e05810284a9f872861364a93fd0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1ff5e3836619aa573c98b889ab3ded6",
+    "url": "/Material-ui-practices/index.html"
+  },
+  {
+    "revision": "e00cf364daf63a5ada1e",
+    "url": "/Material-ui-practices/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "30a4b133b393427d16e0",
+    "url": "/Material-ui-practices/static/js/2.e078ec0d.chunk.js"
+  },
+  {
+    "revision": "e00cf364daf63a5ada1e",
+    "url": "/Material-ui-practices/static/js/main.63a3e83d.chunk.js"
+  },
+  {
+    "revision": "17367119b3c08a75876e",
+    "url": "/Material-ui-practices/static/js/runtime~main.dbb579b7.js"
+  }
+]);
